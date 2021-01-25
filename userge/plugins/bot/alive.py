7def195ab6b5e1d3a8afb4d2eec5d77dd24bb31c
Purple <scryptt@gmail.com>
2021-01-25 09:46:23 -0300

    Revert "Merge remote-tracking branch 'upstream/alpha' into alpha"
    This reverts commit bad5a01a93463efad6942e9882beaf09c511ede7, reversing
    changes made to 6adee64a5f3db6af3bcf670d0e7773c501028293.

--- userge/plugins/bot/alive.py
+++ userge/plugins/bot/alive.py
@@ -1,11 +1,10 @@
 """Fun plugin"""
 
 import asyncio
-from datetime import datetime
 from re import search
 
 from pyrogram import filters
-from pyrogram.errors import BadRequest, FloodWait, Forbidden
+from pyrogram.errors import BadRequest, Forbidden
 from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 
 from userge import Config, Message, get_version, userge, versions
@@ -76,51 +75,25 @@ async def alive_inline(message: Message):
         except (Forbidden, BadRequest) as ex:
             return await message.err(str(ex), del_in=5)
         await message.delete()
-        await asyncio.sleep(200)
+        await asyncio.sleep(450)
         await userge.delete_messages(message.chat.id, y.updates[0].id)
 
 
 if userge.has_bot:
 
     @userge.bot.on_callback_query(filters.regex(pattern=r"^settings_btn$"))
-    async def alive_cb(_, c_q: CallbackQuery):
-        allow = bool(
-            c_q.from_user
-            and (
-                c_q.from_user.id in Config.OWNER_ID
-                or c_q.from_user.id in Config.SUDO_USERS
-            )
-        )
-        if allow:
-            start = datetime.now()
-            try:
-                await c_q.edit_message_text(
-                    Bot_Alive.alive_info(),
-                    reply_markup=Bot_Alive.alive_buttons(),
-                    disable_web_page_preview=True,
-                )
-            except FloodWait as e:
-                await asyncio.sleep(e.x)
-            except BadRequest:
-                pass
-            ping = "𝗣𝗶𝗻𝗴:  🏓  {} sec\n"
-        alive_s = "➕ 𝗘𝘅𝘁𝗿𝗮 𝗣𝗹𝘂𝗴𝗶𝗻𝘀 : {}\n".format(
+    async def alive_cb(_, callback_query: CallbackQuery):
+        alive_s = f"𝗨𝗣𝗧𝗜𝗠𝗘 :  🕑 {userge.uptime}\n"
+        alive_s += "➕ 𝗘𝘅𝘁𝗿𝗮 𝗣𝗹𝘂𝗴𝗶𝗻𝘀 : {}\n".format(
             _parse_arg(Config.LOAD_UNOFFICIAL_PLUGINS)
         )
         alive_s += f"👥 𝗦𝘂𝗱𝗼 : {_parse_arg(Config.SUDO_ENABLED)}\n"
         alive_s += f"🚨 𝗔𝗻𝘁𝗶𝘀𝗽𝗮𝗺 : {_parse_arg(Config.ANTISPAM_SENTRY)}\n"
         if Config.HEROKU_APP and Config.RUN_DYNO_SAVER:
-            alive_s += "⛽️ 𝗗𝘆𝗻𝗼 𝗦𝗮𝘃𝗲𝗿 :  ✅ 𝙴𝚗𝚊𝚋𝚕𝚎𝚍\n"
+            alive_s += f"⛽️ 𝗗𝘆𝗻𝗼 𝗦𝗮𝘃𝗲𝗿 :  ✅ 𝙴𝚗𝚊𝚋𝚕𝚎𝚍\n"
         alive_s += f"💬 𝗕𝗼𝘁 𝗙𝗼𝗿𝘄𝗮𝗿𝗱𝘀 : {_parse_arg(Config.BOT_FORWARDS)}\n"
-        alive_s += f"🛡 𝗣𝗠 𝗚𝘂𝗮𝗿𝗱 : {_parse_arg(not Config.ALLOW_ALL_PMS)}\n"
         alive_s += f"📝 𝗣𝗠 𝗟𝗼𝗴𝗴𝗲𝗿 : {_parse_arg(Config.PM_LOGGING)}"
-        if allow:
-            end = datetime.now()
-            m_s = (end - start).microseconds / 1000
-            await c_q.answer(ping.format(m_s) + alive_s, show_alert=True)
-        else:
-            await c_q.answer(alive_s, show_alert=True)
-        await asyncio.sleep(0.5)
+        await callback_query.answer(alive_s, show_alert=True)
 
 
 def _parse_arg(arg: bool) -> str:
@@ -154,30 +127,29 @@ class Bot_Alive:
     @staticmethod
     def alive_info():
         alive_info = f"""
-<a href="https://telegram.dog/x_xtests"><b>USERGE-X</a> is Up and Running.</b>
+**[LYNX](https://telegram.dog/x_xtests) is Up and Running.**
 
-  🐍   <b>Python :</b>    <code>v{versions.__python_version__}</code>
-  🔥   <b>Pyrogram :</b>    <code>v{versions.__pyro_version__}</code>
-  🧬   <b>𝑿 :</b>    <code>v{get_version()}</code>
-
-<b>{Bot_Alive._get_mode()}</b>    <code>|</code>    🕔  <b>{userge.uptime}</b>
+  •  🐍  **Python**    :    `v{versions.__python_version__}`
+  •  🔥    **Pyro**      :    `v{versions.__pyro_version__}`
+  •  🧬   𝐋𝐘𝐍𝑿     :    `{get_version()}`
+  •  **{Bot_Alive._get_mode()}**    |  🕔: {userge.uptime}
 """
         return alive_info
 
     @staticmethod
     def _get_mode() -> str:
         if RawClient.DUAL_MODE:
-            return "↕️  DUAL"
+            return "↕️   DUAL"
         if Config.BOT_TOKEN:
-            return "🤖  BOT"
-        return "👤  USER"
+            return "🤖   BOT"
+        return "👤   USER"
 
     @staticmethod
     def alive_buttons():
         buttons = [
             [
-                InlineKeyboardButton(text="🔧  SETTINGS", callback_data="settings_btn"),
-                InlineKeyboardButton(text="⚡  REPO", url=Config.UPSTREAM_REPO),
+                InlineKeyboardButton("🔧 SETTINGS", callback_data="settings_btn"),
+                InlineKeyboardButton(text="👾 REPO", url=Config.UPSTREAM_REPO),
             ]
         ]
         return InlineKeyboardMarkup(buttons)
