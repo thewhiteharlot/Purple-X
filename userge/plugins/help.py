9bb3e2a15f1ebc9a7346ccb537a9624e1850bf1a
Purple <scryptt@gmail.com>
2021-02-21 17:06:21 -0300

    Update help.py

--- userge/plugins/help.py
+++ userge/plugins/help.py
@@ -23,6 +23,7 @@ from pyrogram.types import (
 )
 from youtubesearchpython import VideosSearch
 
+
 from userge import Config, Message, get_collection, userge
 from userge.core.ext import RawClient
 from userge.utils import get_file_id, get_response
@@ -31,6 +32,7 @@ from userge.utils import rand_key
 
 from .bot.alive import Bot_Alive
 from .bot.gogo import Anime
+
 from .bot.utube_inline import (
     download_button,
     get_yt_video_id,
@@ -38,6 +40,7 @@ from .bot.utube_inline import (
     result_formatter,
     ytsearch_data,
 )
+
 from .fun.stylish import font_gen
 from .misc.redditdl import reddit_thumb_link
 from .utils.notes import get_inote
