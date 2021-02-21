f4bf2c510e3322f1022eb581eca8f3849f85a38e
Purple <scryptt@gmail.com>
2021-02-21 17:04:57 -0300

    wt

--- userge/plugins/bot/inline_help.py
+++ userge/plugins/bot/inline_help.py
@@ -54,6 +54,10 @@ COMMANDS = {
         "help_txt": "**Write it in Style**\nplugin to decorate text with unicode fonts.\n\n>>>  `stylish [text]`",
         "i_q": "stylish LYNX",
     },
+    "ytdl": {
+        "help_txt": f"**Download YouTube Videos or Audio with Buttons**\nTo Download Video / Audio from youtube with desired quality.\n\n>>>  `ytdl [URL or Text]\n   Non-Inline: {Config.CMD_TRIGGER}iytdl [URL / Text] or [Reply to URL / Text]`",
+        "i_q": "ytdl https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    },
     "spoiler": {
         "help_txt": "**Send Saved Spoiler Via Inline**\n For more info see `.help spoiler`\n\n>>>  `spoiler [ID]`",
         "i_q": "spoiler",
